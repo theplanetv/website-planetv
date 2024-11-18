b8f7e0b2cf02e61cdd6468c9b791f14832a54090
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-18 11:39:26 +0700

    web-svelte: Update footer with SvelteIcon and link

--- web-svelte/src/components/Footer.svelte
+++ web-svelte/src/components/Footer.svelte
@@ -1,7 +1,15 @@
-<style>
-  footer {
-    text-align: center;
-  }
-</style>
+<script>
+	import SvelteIcon from "./icon/SvelteIcon.svelte";
+</script>
 
-<footer>PlanetV - Written in Svelte</footer>
+
+<footer class="mx-auto w-fit text-center flex gap-x-1">
+  <span>PlanetV Website - Built with</span>
+  <a
+    class="transition-all duration-200 group flex items-center hover:cursor-pointer hover:text-orange-svelte"
+    href="https://svelte.dev/"
+  >
+    <SvelteIcon />
+    Svelte
+  </a>
+</footer>
