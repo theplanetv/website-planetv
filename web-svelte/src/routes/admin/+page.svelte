41fd33986930f8678846c00d7930138dcdc9caf4
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-19 22:14:48 +0700

    web-svelte: add admin fetch data code

--- web-svelte/src/routes/admin/+page.svelte
+++ web-svelte/src/routes/admin/+page.svelte
@@ -1,9 +1,16 @@
 <script>
   import { goto } from '$app/navigation';
 
+  let search = $state('');
+  let limit = $state(10);
+  let page = $state(1);
+
+  let count = $state(0);
+  let data = $state([]);
+
   $effect(async () => {
     try {
-      const response = await fetch('http://localhost:14003/api/auth/verify', {
+      const authResponse = await fetch('http://localhost:14003/api/auth/verify', {
         method: 'GET',
         credentials: 'include',
         headers: {
@@ -11,17 +18,63 @@
         }
       });
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        console.error('Authorize failed:', errorData.message);
-        alert(`Authorize failed: ${errorData.message}`);
+      if (!authResponse.ok) {
+        const errorData = await authResponse.json();
+        console.error('Authorization failed:', errorData.message);
+        alert(`Authorization failed: ${errorData.message}`);
         goto('/login', { replaceState: true });
+        return; // Exit if authorization fails
       }
     } catch (error) {
-      console.error('An error occurred during verify:', error);
+      console.error('An error occurred during verification:', error);
       alert('An error occurred. Please try again later.');
+      return; // Exit on error
     }
-  })
+
+    try {
+      const countResponse = await fetch(`http://localhost:14003/api/blogtag/count?search=${search}`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!countResponse.ok) {
+        const errorData = await countResponse.json();
+        console.error('Failed to get count:', errorData.message);
+        return;
+      }
+
+      const countResult = await countResponse.json();
+      count = countResult.data;
+      console.log('Count:', count);
+    } catch (error) {
+      console.error('An error occurred while fetching count:', error);
+    }
+
+    try {
+      const dataResponse = await fetch(`http://localhost:14003/api/blogtag?search=${search}&limit=${limit}&page=${page}`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!dataResponse.ok) {
+        const errorData = await dataResponse.json();
+        console.error('Failed to get data:', errorData.message);
+        return;
+      }
+
+      const dataResult = await dataResponse.json();
+      data = dataResult.data;
+      console.log('Data:', data);
+    } catch (error) {
+      console.error('An error occurred while fetching data:', error);
+    }
+  });
 </script>
 
 <div>
