139323887d8cfbe1bea8a5522c41cb8d57c60805
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-19 21:48:12 +0700

    web-svelte: add go back to login page if user is not logged in

--- web-svelte/src/routes/admin/+page.svelte
+++ web-svelte/src/routes/admin/+page.svelte
@@ -1,3 +1,29 @@
+<script>
+  import { goto } from '$app/navigation';
+
+  $effect(async () => {
+    try {
+      const response = await fetch('http://localhost:14003/api/auth/verify', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error('Authorize failed:', errorData.message);
+        alert(`Authorize failed: ${errorData.message}`);
+        goto('/login', { replaceState: true });
+      }
+    } catch (error) {
+      console.error('An error occurred during verify:', error);
+      alert('An error occurred. Please try again later.');
+    }
+  })
+</script>
+
 <div>
   <h1>Admin</h1>
 </div>
