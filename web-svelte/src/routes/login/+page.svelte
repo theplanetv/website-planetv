d94e41c2b331016c9d93b5871a0678754c4197ca
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-19 09:55:52 +0700

    web-svelte: update login page

--- web-svelte/src/routes/login/+page.svelte
+++ web-svelte/src/routes/login/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Button from "../../components/button/Button.svelte";
   import H1 from "../../components/header/H1.svelte";
+	import ArrowLeftAltIcon from "../../components/icon/ArrowLeftAltIcon.svelte";
 	import KeyIcon from "../../components/icon/KeyIcon.svelte";
 	import KeyOffIcon from "../../components/icon/KeyOffIcon.svelte";
 	import PersonIcon from "../../components/icon/PersonIcon.svelte";
@@ -40,27 +41,19 @@
   }
 </script>
 
-<style>
-  .login-form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    row-gap: 10px;
-  }
-</style>
-
 
 <HorizontalCenterLayout>
   <VerticalCenterLayout>
-    <form class="border rounded-xl p-8 shadow-xl bg-base-100 login-form" onsubmit={submitLogin}>
+    <form class="border rounded-xl p-8 shadow-xl bg-base-100 flex flex-col items-center gap-y-7" onsubmit={submitLogin}>
       <H1>Login</H1>
 
       <div class="flex flex-col gap-y-2">
-        <label class="input input-bordered flex items-center gap-2">
+        <label class="border border-none rounded-xl py-2 px-1 flex items-center gap-2 focus-within:ring-1 focus-within:ring-orange-svelte">
           <PersonIcon />
-          <input class="grow" type="text" placeholder="Username" value={username} onchange={onChangeUsername} />
+          <input class="outline-none grow" type="text" placeholder="Username" value={username} onchange={onChangeUsername} />
         </label>
-        <label class="input input-bordered flex items-center gap-2">
+
+        <label class="border border-none rounded-xl py-2 px-1 flex items-center gap-2 focus-within:ring-1 focus-within:ring-orange-svelte">
           <button class="hover:cursor-pointer" onclick={togglePassword}>
             {#if !showPassword}
               <KeyIcon />
@@ -68,11 +61,13 @@
               <KeyOffIcon />
             {/if}
           </button>
-          <input class="grow" type={showPassword ? "text" : "password"} placeholder="Password" value={password} onchange={onChangePassword}  />
+          <input class="outline-none grow" type={showPassword ? "text" : "password"} placeholder="Password" value={password} onchange={onChangePassword}  />
         </label>
       </div>
 
       <Button type="submit">Login</Button>
+
+      <a href="/" class="flex gap-x-1 transition-colors duration-200 hover:text-orange-svelte"><ArrowLeftAltIcon />Back to Home</a>
     </form>
   </VerticalCenterLayout>
 </HorizontalCenterLayout>
