ea0efd4d833b06e36f30e07176711ec2097b5415
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-11-20 19:03:32 +0700

    web-svelte: update MenuAdmin fit with screen

--- web-svelte/src/components/MenuAdmin.svelte
+++ web-svelte/src/components/MenuAdmin.svelte
@@ -4,14 +4,36 @@
   let { activeOption } = $props();
 </script>
 
-<nav>
-  <h1>Admin dashboard</h1>
-  <div>
-    <button onclick={() => {
-      activeOption = ActiveOptionEnum.BLOGTAG
-    }}>Tag</button>
-    <button onclick={() => {
-      activeOption = ActiveOptionEnum.SETTINGS
-    }}>Settings</button>
+<nav class="h-screen">
+  <h1 class="p-10 text-2xl font-bold text-center">Admin dashboard</h1>
+
+  <div class="flex flex-col">
+    <button
+      class={`py-2 
+        ${activeOption === ActiveOptionEnum.BLOGTAG ?
+        "bg-orange-svelte text-white" :
+        ""
+        }`
+      }
+      onclick={() => {
+        activeOption = ActiveOptionEnum.BLOGTAG
+      }}
+    >
+      Tag
+    </button>
+
+    <button
+    class={`py-2 
+      ${activeOption === ActiveOptionEnum.SETTINGS ?
+      "bg-orange-svelte text-white" :
+      ""
+      }`
+    }
+      onclick={() => {
+        activeOption = ActiveOptionEnum.SETTINGS
+      }}
+    >
+      Settings
+    </button>
   </div>
 </nav>
