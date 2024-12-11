4c51ec9870e2e36c21b645aa0b8049efb45608b4
goatastronaut0212 <goatastronaut0212@outlook.com>
2024-12-11 17:48:43 +0700

    web-svelte: update table css

--- web-svelte/src/components/table/TableAdmin.svelte
+++ web-svelte/src/components/table/TableAdmin.svelte
@@ -6,48 +6,50 @@
   let { columns, data, formStatus = $bindable(), inputValue = $bindable(), } = $props();
 </script>
 
-<table>
-  <thead>
-    <tr>
-      {#each Object.keys(columns) as key}
-        <th class="bg-slate-200 p-2">{columns[key]}</th>
-      {/each}
-      <th class="bg-slate-200 p-2">Actions</th>
-    </tr>
-  </thead>
-
-  <tbody>
-    {#if data.length > 0}
-      {#each data as row}
-        <tr>
-          {#each Object.keys(columns) as key}
-            <td>{row[key]}</td>
-          {/each}
-          <td>
-            <button type="button"
-              onclick={() => {
-                formStatus = FormStatusEnum.EDIT;
-                inputValue = { ...row };
-              }}
-            >
-              <BaselineModeEdit />
-            </button>
-
-            <button type="button"
-              onclick={() => {
-                formStatus = FormStatusEnum.DELETE;
-                inputValue = { ...row };
-              }}
-            >
-              <BaselineDelete />
-            </button>
-          </td>
-        </tr>
-      {/each}
-    {:else}
-      <tr>
-        <td class="text-center" colspan={Object.keys(columns).length + 1}>No result</td>
+<div class="rounded-lg overflow-hidden border border-slate-200">
+  <table class="w-full border-collapse">
+    <thead>
+      <tr class="divide-x divide-white">
+        {#each Object.keys(columns) as key}
+          <th class="bg-slate-200 p-2">{columns[key]}</th>
+        {/each}
+        <th class="bg-slate-200 p-2">Actions</th>
       </tr>
-    {/if}
-  </tbody>
-</table>
+    </thead>
+
+    <tbody>
+      {#if data.length > 0}
+        {#each data as row}
+          <tr>
+            {#each Object.keys(columns) as key}
+              <td class="p-2 border border-slate-200">{row[key]}</td>
+            {/each}
+            <td class="p-2 border border-slate-200">
+              <button type="button"
+                onclick={() => {
+                  formStatus = FormStatusEnum.EDIT;
+                  inputValue = { ...row };
+                }}
+              >
+                <BaselineModeEdit />
+              </button>
+
+              <button type="button"
+                onclick={() => {
+                  formStatus = FormStatusEnum.DELETE;
+                  inputValue = { ...row };
+                }}
+              >
+                <BaselineDelete />
+              </button>
+            </td>
+          </tr>
+        {/each}
+      {:else}
+        <tr>
+          <td class="text-center p-2 border border-slate-200" colspan={Object.keys(columns).length + 1}>No result</td>
+        </tr>
+      {/if}
+    </tbody>
+  </table>
+</div>
