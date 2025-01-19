ef14f627dd73e50c67fad3e01738368f42434fee
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-01-19 21:31:40 +0700

    web-next: add blogtag api route and update ResponseEnum

---
+++ web-next/src/libs/api/blogtag.ts
@@ -0,0 +1,49 @@
+import { ResponseEnum } from "@/libs/enum";
+import { API_URL } from "@/libs/config";
+
+type ReturnData = {
+  data: number
+  success: boolean
+}
+
+async function count(): Promise<ReturnData> {
+  try {
+    const response = await fetch(`${API_URL}/blog/tags/count?search=`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    if (!response.ok) {
+      return {
+        data: 0,
+        success: false,
+      };
+    }
+
+    const jsonResponse = await response.json();
+
+    if (jsonResponse.message === ResponseEnum.GET_DATA_SUCCESS) {
+      return {
+        data: jsonResponse.data,
+        success: true,
+      };
+    } else {
+      return {
+        data: 0,
+        success: false,
+      };
+    }
+  } catch (error) {
+    return {
+      data: 0,
+      success: false,
+    };
+  }
+}
+
+export default {
+  count
+}
--- web-next/src/libs/enum.ts
+++ web-next/src/libs/enum.ts
@@ -12,5 +12,6 @@ export enum MenuAdminEnum {
 
 export enum ResponseEnum {
   AUTH_SUCCESS = "Authorize success!",
-  LOGIN_SUCCESS = "Login success!"
+  LOGIN_SUCCESS = "Login success!",
+  GET_DATA_SUCCESS = "Get data success!",
 }
