6ead435fb9985ea4edfb426291ea16a8b6d72859
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-03-14 23:48:16 +0700

    web-sveltekit: init

---
+++ web-sveltekit/src/components/menu/Menu.svelte
@@ -0,0 +1,15 @@
+<script>
+  import MingCuteBook2Line from '../icons/MingCute-Book2Line.svelte';
+  import MingCuteHome3Line from '../icons/MingCute-Home3Line.svelte';
+</script>
+
+<nav class="menu menu-horizontal rounded-box bg-base-200 my-5 flex p-1">
+  <a href="/" class="tooltip tooltip-bottom rounded-box p-2 hover:bg-gray-200" data-tip="Home"
+    ><MingCuteHome3Line /></a
+  >
+  <a
+    href="/posts/"
+    class="tooltip tooltip-bottom rounded-box p-2 hover:bg-gray-200"
+    data-tip="Blog Post"><MingCuteBook2Line /></a
+  >
+</nav>
---
+++ web-sveltekit/src/routes/posts/[title]/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import SvelteMarkdown from '@humanspeak/svelte-markdown'
+  
+  import Footer from '../../../components/footer/Footer.svelte';
+  import PageLayout from '../../../components/layout/PageLayout.svelte';
+  import Menu from '../../../components/menu/Menu.svelte';
+
+  const source = `
+# This is a header
+
+This is a paragraph with **bold** and <em>mixed HTML</em>.
+
+* List item with \`inline code\`
+* And a [link](https://svelte.dev)
+  * With nested items
+  * Supporting full markdown
+
+| Type       | Content                                 |
+| ---------- | --------------------------------------- |
+| Nested     | <div>**bold** and _italic_</div>        |
+| Mixed List | <ul><li>Item 1</li><li>Item 2</li></ul> |
+| Code       | <code>\`inline code\`</code>              |
+`
+</script>
+
+<PageLayout>
+  <Menu />
+
+  <article class="prose">
+    <SvelteMarkdown {source} />
+  </article>
+
+  <Footer />
+</PageLayout>
