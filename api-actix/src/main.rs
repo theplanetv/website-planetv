03ed02c49d05f2807a2fc57e3eefd5cee2162583
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-03-15 15:37:38 +0700

    api-actix: init

---
+++ api-actix/src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use env_logger::Env;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
